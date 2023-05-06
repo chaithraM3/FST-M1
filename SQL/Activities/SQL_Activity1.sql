6d8d348a60b99420e450e615d3ae424aab5f24e1
chaithraM3 <129042469+chaithraM3@users.noreply.github.com>
2023-05-07 00:06:55 +0530

    Add files via upload

---
+++ SQL/Activities/SQL_Activity1.sql
@@ -0,0 +1,14 @@
+REM   Script: Activity 1
+REM   IBM SDET SQL-Activity1
+
+create table salesman( 
+    salesman_id int, 
+    salesman varchar2(50), 
+    salesman_city varchar2(20), 
+    commission int 
+    );
+
+desc salesman
+
+
+
